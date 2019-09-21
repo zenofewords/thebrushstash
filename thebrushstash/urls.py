421fc0003169b41b5c3aab4f2af6ff8121e1f43b
zenofewords <zen.dominik@gmail.com>
2019-09-21 19:05:28 +0200

    Setup project

---
+++ thebrushstash/urls.py
@@ -0,0 +1,29 @@
+"""
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'The Brush Stash'
+admin.site.site_title = 'The Brush Stash'
+
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var path = require('path')
+var WebpackBundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+  entry: {
+    shop: './static/javascript/shop',
+  },
+  output: {
+    filename: '[name]_[hash].js',
+    path: path.resolve(__dirname, 'staticfiles/bundles'),
+    publicPath: 'http://localhost:3000/static/bundles/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[hash].css',
+    }),
+    new WebpackBundleTracker({
+      filename: './webpack-stats.json'
+    }),
+  ],
+  mode: 'development',
+}
