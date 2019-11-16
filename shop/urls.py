e741600fbec05a3cd30617a6bb9391006970463e
zenofewords <zen.dominik@gmail.com>
2019-11-16 15:51:30 +0100

    Add trailing slashes

--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,6 @@ urlpatterns = (
     path('products/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
     path('review-bag/', ReviewBagView.as_view(), name='review-bag'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('purchase-complete', PurchaseCompleteView.as_view(), name='purchase-complete'),
-    path('purchase-cancelled', PurchaseCancelledView.as_view(), name='purchase-cancelled'),
+    path('purchase-complete/', PurchaseCompleteView.as_view(), name='purchase-complete'),
+    path('purchase-cancelled/', PurchaseCancelledView.as_view(), name='purchase-cancelled'),
 )
