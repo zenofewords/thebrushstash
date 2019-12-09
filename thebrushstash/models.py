8f5419d6654c02fe63eafaae259d8a5ec54149fe
zenofewords <zen.dominik@gmail.com>
2019-12-10 00:02:35 +0100

    Update country model

--- thebrushstash/models.py
+++ thebrushstash/models.py
@@ -9,13 +9,10 @@ from thebrushstash.mixins import (
 
 class Country(PublishedMixin):
     name = models.CharField(max_length=500)
-    name_cro = models.CharField(max_length=500)
+    name_cro = models.CharField(max_length=500, blank=True)
     slug = models.CharField(max_length=500)
     shipping_cost = models.DecimalField(
         verbose_name='Shipping cost', max_digits=14, decimal_places=2, blank=True, null=True,
-    )
-    region = models.ForeignKey(
-        'thebrushstash.Region', on_delete=models.deletion.CASCADE, blank=True, null=True,
     )
 
     class Meta:
