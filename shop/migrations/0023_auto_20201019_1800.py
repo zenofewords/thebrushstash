7d569f385b2e4aa98d9f64d8258e6369d24cd120
zenofewords <zen.dominik@gmail.com>
2020-10-19 20:16:03 +0200

    Split data migration

--- shop/migrations/0023_auto_20201019_1800.py
+++ shop/migrations/0023_auto_20201019_1800.py
@@ -4,17 +4,6 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-def copy_price_to_old_price(apps, schema_editor):
-    Product = apps.get_model('shop', 'Product')
-
-    for product in Product.objects.all():
-        product.old_price_hrk = product.price_hrk
-        product.old_price_usd = product.price_usd
-        product.old_price_gbp = product.price_gbp
-        product.old_price_eur = product.price_eur
-        product.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -63,9 +52,5 @@ class Migration(migrations.Migration):
             model_name='product',
             name='custom_label',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='shop.customlabel'),
-        ),
-        migrations.RunPython(
-            code=copy_price_to_old_price,
-            reverse_code=migrations.operations.special.RunPython.noop,
         ),
     ]
